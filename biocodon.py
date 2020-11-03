c7925cbad4df7523e455db31715f5ce458edb590
Benjamin Fang <benjaminfang.ol@outlook.com>
2020-11-03 14:39:04 +0800

    codon information

--- biocodon.py
+++ biocodon.py
@@ -23,3 +23,6 @@ CODON_AA={'TTT':'F','TTC':'F',
 
 
 base_complement = {'A':'T', 'T':'A', 'G':'C', 'C':'G', 'N':'N', 'a':'t', 't':'a', 'c':'g', 'g':'c', 'n':'n'}
+
+start_codon = ['ATG']
+stop_codon = ['TAA', 'TAG', 'TGA']
