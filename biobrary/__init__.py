10c1ff485c8a1faa3bf1718dd6298b842a2cc8f9
Benjamin Fang <benjaminfang.ol@outlook.com>
2021-01-18 17:40:42 +0800

    bugfix

--- biobrary/__init__.py
+++ biobrary/__init__.py
@@ -2,5 +2,4 @@ from .bioparser import *
 from .biocodon import *
 from .CircleNode import *
 from .amino_acids_mw import *
-from .EDNAFULL import *
-from .EBLOSUM62 import *
+from .score_matrix import *
