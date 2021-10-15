2180001d3776ac9255b5212308fde3243cec9287
Benjamin Fang <benjaminfang.ol@outlook.com>
2021-10-15 13:12:17 +0800

    move setup configuration directives to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
 import setuptools
 
 setuptools.setup(
-    name="biobrary",
-    version="0.0.2",
-    description="library for bioinformatics.",
-    long_description="biobrary\nA library for bioinformatics.\n",
-    long_description_content_type="text/plain",
-    url="https://github.com/benjaminfang/biobrary",
-    author="Benjamin Fang",
-    author_email="benjaminfang.ol@outlook.com",
-    license='MIT',
-    python_requires=">=3.7",
-    classifers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: MIT License",
-        "Intended Audience :: Science/Research",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
-    ],
-    keywords="bioinformatics, file_parser",
     packages=setuptools.find_packages(),
 )
