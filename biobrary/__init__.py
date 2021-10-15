5bba96b27fc7dbe9f663262a991fc76f3e2486b7
Benjamin Fang <benjaminfang.ol@outlook.com>
2021-10-15 14:39:40 +0800

    chang import module

--- biobrary/__init__.py
+++ biobrary/__init__.py
@@ -1,5 +1,7 @@
-from .bioparser import *
-from .biocodon import *
-from .CircleNode import *
-from .amino_acids_mw import *
-from .score_matrix import *
+import biobrary.bioparser as bioparser
+import biobrary.biocodon as biocodon
+import biobrary.amino_acids_mw as amino_acids_mw
+
+import biobrary.CircleNode as CircleNode
+
+import biobrary.score_matrix as score_matrix
